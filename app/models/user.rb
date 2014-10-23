e57086abfda07e80449b6e9185a19fcb32d179c4
Jake Goulding <jake.goulding@gmail.com>
2014-10-23 11:47:11 -0400

    second failure - Cannot visit Arel::SelectManager
    User.with_has_address.count

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,23 @@ class User < ActiveRecord::Base
              class_name: 'User',
              inverse_of: :children
 
+  has_many :addresses,
+           inverse_of: :user
+
   def self.x(user)
     active_children = user.children.active
 
     joins { active_children.as('active_users').on { id.in(active_users.id) }.outer }
       .select { ["users.*", active_users.id.as('calc_active')] }
+  end
+
+  def self.has_address
+    joins { addresses }
+  end
+
+  def self.with_has_address
+    a = User.has_address
+    joins { a.as('addressable').on { id.eq(addressable.id) }.outer }
+      .select { ["users.*", addressable.id.as('addressable')] }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141023145725) do
+ActiveRecord::Schema.define(version: 20141023153214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
